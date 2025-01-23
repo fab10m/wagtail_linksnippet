bfc493ded317a3b6239f32d6c7bf1c9ec35c5459
Fabio Marras <fabio.marras@fintastico.com>
2025-01-23 10:55:12 +0100

    Update the version to 1.0.1 and configure the deployment.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wagtail_linksnippet',
-    version='0.1.0',
+    version='1.0.1',
     packages=find_packages(),
     include_package_data=True,
     description='A Wagtail extension to open custom chooser from RichText Editor.',
